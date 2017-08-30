376432a7c887eb374a25a3f3d482473116203db6
luyi <435977170@qq.com>
2017-08-30 17:12:25 +0800

    first upload

---
+++ tutorail/tutorial.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np 
+
+x_input = tf.constant(2, tf.int16)
+w_input = tf.constant(4, tf.float16)
+
+graph = tf.Graph()
+with graph.as_default():
+    a = tf.Variable(8, tf.int16)
+    b = tf.Variable(tf.zeros([2,2]))
+
+with tf.Session(graph=graph) as sess:
+    tf.global_variables_initializer().run()
+    print(graph)
+    print(sess.run(a))
+    print(sess.run(b))
+
+a = tf.constant(2, tf.int16)
+b = tf.constant(4, tf.int16)
+x = tf.Variable(6, tf.int16)
+y = tf.Variable(8, tf.int16)
+sess1 = tf.Session()
+result = a+b
+print(result)
+print(sess1.run(result))
+sess1.run(tf.global_variables_initializer())
+print(x, y)
+print(sess1.run(x), sess1.run(y))
+sess1.close()
+
+#---------------------------------------------------------------------------------------------
+x = tf.Variable(tf.truncated_normal([256*256, 10]))
+w = tf.Variable(tf.ones([10, 10]))
+print(x.get_shape().as_list())
+print(w.get_shape().as_list())
+
+x = tf.placeholder(tf.float32, [1, 2])
+x1 = tf.constant([[0.7, 0.9]])
+w = tf.Variable(tf.random_normal([1, 2], mean=1.0, stddev=2.0))
+y = x + w
+y1 = x1 + w
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+print(sess.run(y, feed_dict={x:[[0.6, 0.8]]}))
+print(sess.run(y1))
+
+#-----------------------------------------------------------------------------------------------
+points_1 = [[1,2], [2,3], [3,4], [4,5]]
+points_2 = [[3,4], [4,5], [5,6], [6,7]]
+input_x = np.array([np.array(elem).reshape(1,2) for elem in points_1])
+input_y = np.array([np.array(elem).reshape(1,2) for elem in points_2])
+
+graph = tf.Graph()
+with graph.as_default():
+    x = tf.placeholder(tf.float16, [1,2])
+    y = tf.placeholder(tf.float16, [1,2])
+    def calc(xi, yi):
+        diff = tf.subtract(xi, yi)
+        pows = tf.pow(diff, tf.constant(2.0, tf.float16, [1,2]))
+        adds = tf.reduce_sum(pows)
+        result = tf.sqrt(adds)
+        return result
+    ret = calc(x, y)
+
+with tf.Session(graph=graph) as sess:
+    for i in range(len(points_1)):
+        x_i = input_x[i]
+        y_i = input_y[i]
+        feed_dict = {x:x_i, y:y_i}
+        print('The %d result is %f' % (i, sess.run(ret, feed_dict=feed_dict)))
+
+#---------------------------------------------------------------------------------------------
+batch_size = 10
+
+graph = tf.Graph()
+with graph.as_default():
+    w1 = tf.Variable(tf.random_normal([2, 3]))
+    w2 = tf.Variable(tf.random_normal([3, 1]))
+    input_x = tf.placeholder(tf.float32, [None, 2])
+    input_y = tf.placeholder(tf.float32, [None, 1])
+
+    a = tf.matmul(input_x, w1)
+    relu_a = tf.nn.relu(a)
+    b = tf.matmul(relu_a, w2)
+    cross_entr_loss = -tf.reduce_mean(input_y*tf.clip_by_value(b, 1e-10, 1.0))
+    train = tf.train.AdamOptimizer().minimize(cross_entr_loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
